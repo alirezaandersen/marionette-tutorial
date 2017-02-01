84f393b755000196e26472597f56493cc141d82c
Alireza Andersen <alireza.andersen@gmail.com>
2017-02-01 10:04:45 -0700

    driver.js updated with layoutview

---
+++ app/driver.js
@@ -0,0 +1,19 @@
+var Backbone = require('backbone');
+var Marionette = require('backbone.marionette');
+
+
+var TodoList = Marionette.LayoutView.extend({
+  el: '#app-hook',
+  template: require('./templates/layout.html')
+});
+
+var todo = new TodoList({
+  model: new Backbone.Model({
+    items: [
+      {assignee: 'Scott', text: 'Write a book about Marionette'},
+      {assignee: 'Andrew', text: 'Do some coding'}
+    ]
+  })
+});
+
+todo.render();
